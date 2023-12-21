9394f7c8ae563d3dce55ce4dbf0625b8c2dfb5a4
Kenneth Choi <kennethichoi@gmail.com>
2023-12-20 16:03:54 -0800

    Implement sorting via route params

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React from 'react';
 import './App.css';
 import LeaderBoard from './Component/LeaderBoard';
 import 'h8k-components';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const title = "SPA - LeaderBoard";
+const title = 'SPA - LeaderBoard';
 
 function App() {
-	return (
-		<div className="App">
-			<h8k-navbar header={title}></h8k-navbar>
-			<LeaderBoard />
-		</div>
-	);
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h8k-navbar header={title}></h8k-navbar>
+        <Switch>
+          <Route path="/:sortKey">
+            <LeaderBoard />
+          </Route>
+          <Route path="/">
+            <LeaderBoard />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/Component/LeaderBoard.js
+++ src/Component/LeaderBoard.js
@@ -1,42 +1,105 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { response } from '../response';
 
-const index = 0;
+import { useParams, Link } from 'react-router-dom';
+
+const SORTKEY = {
+  RANK: 'rank',
+  NAME: 'name',
+  POINTS: 'points',
+  AGE: 'age',
+};
 
 function LeaderBoard(props) {
-	const { rank, name, points, age} = response.list[index];
-	return (
-		<div className="text-center mt-50">
-			<div>
-				<div>
-					<button data-testid="route-rank" className='outlined' type="button">Rank</button>
-					<button data-testid="route-name" className='outlined' type="button">Name</button>
-					<button data-testid="route-points" className='outlined' type="button">Points</button>
-					<button data-testid="route-age" className='outlined' type="button">Age</button>
-				</div>
-			</div>
-			<div className="card mx-auto pb-20 mb-30" style={{ width: '50%' }}>
-				<table className="mt-50" data-testid="app-table">
-					<thead>
-						<tr>
-							<th>Rank</th>
-							<th>Name</th>
-							<th className="numeric">Points</th>
-							<th className="numeric">Age</th>
-						</tr>
-					</thead>
-					<tbody data-testid="app-tbody">
-						<tr key={rank}>
-							<td data-testid={`rank-${index}`}>{rank}</td>
-							<td data-testid={`name-${index}`}>{name}</td>
-							<td data-testid={`points-${index}`} className="numeric">{points}</td>
-							<td data-testid={`age-${index}`} className="numeric">{age}</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	);
+  const data = response['list'];
+  const { sortKey } = useParams();
+
+  const sortedData = useMemo(() => {
+    if (!sortKey) return data;
+
+    return data.sort((a, b) => {
+      if (sortKey === SORTKEY.RANK) {
+        if (Number(a.rank) < Number(b.rank)) return -1;
+        return 1;
+      } else if (sortKey === SORTKEY.NAME) {
+        if (a.name < b.name) return -1;
+        return 1;
+      } else if (sortKey === SORTKEY.POINTS) {
+        if (Number(a.points) < Number(b.points)) return -1;
+        return 1;
+      } else if (sortKey === SORTKEY.AGE) {
+        if (Number(a.age) < Number(b.age)) {
+          return -1;
+        } else if (Number(a.age) > Number(b.age)) {
+          return 1;
+        } else {
+          if (Number(a.rank) < Number(b.rank)) return -1;
+          return 1;
+        }
+      } else {
+        return -1;
+      }
+    });
+  }, [data, sortKey]);
+
+  return (
+    <div className="text-center mt-50">
+      <div>
+        <div>
+          <Link to="/rank">
+            <button data-testid="route-rank" className="outlined" type="button">
+              Rank
+            </button>
+          </Link>
+          <Link to="/name">
+            <button data-testid="route-name" className="outlined" type="button">
+              Name
+            </button>
+          </Link>
+          <Link to="/points">
+            <button
+              data-testid="route-points"
+              className="outlined"
+              type="button"
+            >
+              Points
+            </button>
+          </Link>
+          <Link to="/age">
+            <button data-testid="route-age" className="outlined" type="button">
+              Age
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className="card mx-auto pb-20 mb-30" style={{ width: '50%' }}>
+        <table className="mt-50" data-testid="app-table">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th className="numeric">Points</th>
+              <th className="numeric">Age</th>
+            </tr>
+          </thead>
+          <tbody data-testid="app-tbody">
+            {sortedData.map((row, idx) => (
+              <tr key={row.rank}>
+                <td data-testid={`rank-${idx}`}>{row.rank}</td>
+                <td data-testid={`name-${idx}`}>{row.name}</td>
+                <td data-testid={`points-${idx}`} className="numeric">
+                  {row.points}
+                </td>
+                <td data-testid={`age-${idx}`} className="numeric">
+                  {row.age}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default LeaderBoard;
